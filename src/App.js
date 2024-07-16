1742c8ccb82c0fd616ff8ae52b8921ad5f47317f
Kimuli Arnold <kimarnold.dev@gmail.com>
2024-07-16 15:38:26 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import UserPage from './components/UserPage';
 import AdminDashboard from './components/AdminDashboard';
+
+import UserRegister from './components/UserRegister';
+
 import './App.css';
 
 const App = () => {
@@ -12,7 +15,10 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/userLogin" element={<UserPage />} />
-          <Route path="/userRegister" element={<UserPage />} />
+          <Route path="/userRegister" element={<UserRegister />} />
+
+
+
           <Route path="/adminLogin" element={<AdminDashboard />} />
           <Route path="/adminRegister" element={<AdminDashboard />} />
         </Routes>
--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './styles/UserPage.css';
+import '../styles/UserPage.css';
 
 const UserPage = () => {
   const [formData, setFormData] = useState({
